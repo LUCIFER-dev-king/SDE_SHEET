d76389865df4d4e02746af1d70f1791cc343e41f
LUCIFER-dev-king <nihaljesiya@gmail.com>
2021-09-13 19:44:29 +0530

    added natruxzero sol

---
+++ day2/01_MatrxiSetZero.cpp
@@ -0,0 +1,93 @@
+// https://leetcode.com/problems/set-matrix-zeroes/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Straight forward solution. Tc->O(m*n + m + n) sp O(m+n)
+class Solution
+{
+public:
+  void setZeroes(vector<vector<int>> &matrix)
+  {
+    int m = matrix.size();
+    int n = matrix[0].size();
+    vector<vector<int>> copy;
+    copy = matrix;
+    for (int i = 0; i < m; i++)
+    {
+      for (int j = 0; j < n; j++)
+      {
+        if (matrix[i][j] == 0)
+        {
+          for (int k = 0; k < m; k++)
+          {
+            copy[k][j] = 0;
+          }
+          for (int k = 0; k < n; k++)
+          {
+            copy[i][k] = 0;
+          }
+        }
+      }
+    }
+
+    matrix = copy;
+  }
+};
+
+// Optimal solution. Tc-> O(n2) sp->O(1)
+class Solution1
+{
+public:
+  void setZeroes(vector<vector<int>> &matrix)
+  {
+    int m = matrix.size();
+    int n = matrix[0].size();
+    bool isFirst = false;
+    for (int i = 0; i < m; i++)
+    {
+      if (matrix[i][0] == 0)
+        isFirst = true;
+      for (int j = 1; j < n; j++)
+      {
+        if (matrix[i][j] == 0)
+        {
+          matrix[i][0] = 0;
+          matrix[0][j] = 0;
+        }
+      }
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+      for (int j = 0; j < n; j++)
+      {
+        cout << matrix[i][j] << " ";
+      }
+      cout << endl;
+    }
+
+    for (int i = 1; i < m; i++)
+    {
+      for (int j = 1; j < n; j++)
+      {
+        if (matrix[i][0] == 0 || matrix[0][j] == 0)
+        {
+          matrix[i][j] = 0;
+        }
+      }
+    }
+
+    if (matrix[0][0] == 0)
+    {
+      for (int i = 0; i < n; i++)
+        matrix[0][i] = 0;
+    }
+
+    if (isFirst)
+    {
+      for (int i = 0; i < m; i++)
+        matrix[i][0] = 0;
+    }
+  }
+};
